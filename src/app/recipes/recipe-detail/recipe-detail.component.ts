f7112f74080e61f4a8cb7614056890170ade421e
Jacob Dozier <jacobadozier@gmail.com>
2023-01-05 10:43:43 -0600

    Refactoring event emitters/listeners to utilize services.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,7 +12,14 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  onAddToShoppingList(recipe: Recipe) {
+    // recipe.ingredients.forEach((ingredient) => {
+    //   this.shoppingListService.addIngredient(ingredient);
+    // });
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {
   }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,38 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel', 
+      'A super-tasty Schnitzel - just awesome', 
+      'https://upload.wikimedia.org/wikipedia/commons/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Quad Stacker Burger', 
+      'Quad Stacker from Burger King', 
+      'https://upload.wikimedia.org/wikipedia/commons/4/43/Burger_King_Quad_Stacker_cheeseburger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 2)
+      ])
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
+  constructor(private shoppingListService: ShoppingListService) {}
+}
