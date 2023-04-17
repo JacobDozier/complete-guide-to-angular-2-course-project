8b9697ec0340e34000708d695fef436b7a9585dc
Jacob Dozier <jacobadozier@gmail.com>
2023-04-17 13:49:34 -0500

    Slight refactor to prevent ongoing subscription to user in router guard.

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 import { AuthService } from './auth.service';
 
@@ -16,7 +16,9 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     router: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      return this.authService.user.pipe(map(user => {
+      return this.authService.user.pipe(
+        take(1),
+        map(user => {
         const isAuth = !!user;
         if (isAuth) {
           return true;
