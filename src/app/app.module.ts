86d47623c69ca28e8035e90b45b48461d49b4200
Jacob Dozier <jacobadozier@gmail.com>
2023-04-20 13:13:04 -0500

    Lazy loading the auth and shopping list features.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,7 @@ import { HeaderComponent } from './header/header.component';
 // Directives
 // Services
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -21,11 +19,9 @@ import { AuthModule } from './auth/auth.module';
   ],
   imports: [
     AppRoutingModule,
-    AuthModule,
     BrowserModule,
     HttpClientModule,
     SharedModule,
-    ShoppingListModule
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
